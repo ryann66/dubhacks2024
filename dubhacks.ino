bf090d7b312c95379b7e60ca844fd2f372c20609
Ryan N <ryann66@cs.washington.edu>
2024-10-12 13:47:33 -0700

    Second commit?

--- dubhacks.ino
+++ dubhacks.ino
@@ -1,28 +1,50 @@
-/* @file CustomKeypad.pde
-|| @version 1.0
-|| @author Alexander Brevig
-|| @contact alexanderbrevig@gmail.com
-||
-|| @description
-|| | Demonstrates changing the keypad size and key values.
-|| #
-*/
+#include <LiquidCrystal.h>
 #include <Keypad.h>
+
+#define BUFLEN 256
+
+enum keynum : char {
+  STAR = 10, POUND = 11, A = 12, B = 13, C = 14, D = 15
+};
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
-char hexaKeys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+keynum hexaKeys[ROWS][COLS] = {
+  {1,2,3,A},
+  {4,5,6,B},
+  {7,8,9,C},
+  {STAR,0,POUND,D}
 };
 byte rowPins[ROWS] = {43, 41, 39, 37}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {40, 38, 36, 34}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+char buf[BUFLEN];
+
+char stkchar;
+uint8_t stklen;
+
+/*
+ * Flushes the keystroke stack, appending the created char to output
+ * Does nothing if stack is empty
+ */
+void flushStack();
+
+/*
+ * Prints the buffer to the screen
+ * Also prints the last char on the stack
+ */
+void printScreen();
+
+/**
+ * Adds a non-operator character
+ */
+void addCharacter(char c);
 
 void setup(){
   Serial.begin(9600);
